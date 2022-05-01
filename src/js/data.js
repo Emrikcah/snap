83bb26a105c139e95b03d03aad64f4404278fb99
Emrikcah <curryandr3@gmail.com>
2022-05-01 10:09:54 -0400

    add features data

---
+++ src/js/data.js
@@ -0,0 +1,22 @@
+const features = [
+   {
+      id: 1,
+      images: '../images/icon-todo.svg',
+      text: 'TodoList'
+   },
+   {
+      id: 2,
+      images: '../images/icon-calendar.svg',
+      text: 'Calendar'
+   },
+   {
+      id: 3,
+      images: '../images/icon-reminders.svg',
+      text: 'Reminders'
+   },
+   {
+      id: 4,
+      images: '../images/icon-planning.svg',
+      text: 'Planning'
+   },
+]
